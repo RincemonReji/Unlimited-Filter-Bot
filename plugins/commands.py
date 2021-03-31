9ce50cb738988a2e5e10dfc6c1ef0fc3f946571c
RINCEMON REJI <70466997+RincemonReji@users.noreply.github.com>
2021-03-31 22:49:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Developer", url="https://t.me/filmsandtrends_owner"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/MF_Malayalam"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/MoviesFactory_Group")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "Support Group", url="https://t.me/MoviesFactory_Group")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
